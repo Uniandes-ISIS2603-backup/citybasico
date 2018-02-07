671bcde4d0a2151754b769b0afa1cec7b0b068f2
Rubby Casallas <rcasalla@uniandes.edu.co>
2018-02-07 13:20:32 -0500

    finetuning formato Signed-off-by:Rubby Casallas <rcasalla@uniandes.edu.co>

--- cities-web/src/main/java/co/edu/uniandes/csw/cities/resources/CityResource.java
+++ cities-web/src/main/java/co/edu/uniandes/csw/cities/resources/CityResource.java
@@ -54,7 +54,7 @@ import javax.ws.rs.Produces;
  * RequestScoped: Inicia una transacción desde el llamado de cada método (servicio). 
  * </pre>
  * @author ISIS2603  
- * @version
+ * @version 1.0
  */
 @Path("cities")
 @Produces("application/json")
@@ -63,19 +63,25 @@ import javax.ws.rs.Produces;
 public class CityResource {
 
     /**
-     * <h1>POST /api/cities : Crear ciudad</h1>
-     * <p>Cuerpo de peticion: JSON {@link CityDetailDTO}</p>
+     * <h1>POST /api/cities : Crear una ciudad.</h1>
      * 
-     * <p>Crea una nueva ciudad con la informacion que se recibe en el cuerpo de la peticion.</p>
+     * <pre>Cuerpo de petición: JSON {@link CityDetailDTO}.
      * 
-     * Codigos de respuesta<br>
-     * <code style="color: mediumseagreen; background-color: #eaffe0;">200 OK
-     * Se crea la nueva ciudad y se regresa un objeto identico con un id auto-generado por la base de datos.</code>
-     * <code style="color: #c7254e; background-color: #f9f2f4;">412 Precodition Failed
-     * Ya existe la ciudad.</code>
+     * Crea una nueva ciudad con la informacion que se recibe en el cuerpo 
+     * de la petición y se regresa un objeto identico con un id auto-generado 
+     * por la base de datos.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Creó la nueva ciudad .
+     * </code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 412 Precodition Failed: Ya existe la ciudad.
+     * </code>
+     * </pre>
      * @param city {@link CityDetailDTO} - La ciudad que se desea guardar.
      * @return JSON {@link CityDetailDTO}  - La ciudad guardada con el atributo id autogenerado.
-     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de logica que se genera cuando ya existe la ciudad.
+     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de lógica que se genera cuando ya existe la ciudad.
      */
     @POST
     public CityDetailDTO createCity(CityDetailDTO city) throws BusinessLogicException {
@@ -83,14 +89,15 @@ public class CityResource {
     }
 
     /**
-     * <h1>GET /api/cities : Obtener ciudades</h1>
+     * <h1>GET /api/cities : Obtener todas las ciudades.</h1>
      * 
-     * <p>Busca y devuelve todas las ciudades que existen en la aplicacion.</p>
+     * <pre>Busca y devuelve todas las ciudades que existen en la aplicacion.
      * 
-     * Codigos de respuesta<br>
-     * <code style="color: mediumseagreen; background-color: #eaffe0;">200 OK
-     * Devuelve todas las ciudades de la aplicacion.</code> 
-     * @return JSONArray {@link CityDetailDTO} - Las ciudades encontradas en la aplicacion.
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve todas las ciudades de la aplicacion.</code> 
+     * </pre>
+     * @return JSONArray {@link CityDetailDTO} - Las ciudades encontradas en la aplicación. Si no hay ninguna retorna una lista vacía.
      */
     @GET
     public List<CityDetailDTO> getCities() {
@@ -98,16 +105,19 @@ public class CityResource {
     }
 
     /**
-     * <h1>GET /api/cities/{id} : Obtener ciudad por id</h1>
+     * <h1>GET /api/cities/{id} : Obtener ciudad por id.</h1>
      * 
-     * <p>Busca la ciudad con el ID asociado recibido en la URL y la devuelve</p>
+     * <pre>Busca la ciudad con el id asociado recibido en la URL y la devuelve.
      * 
-     * Codigos de respuesta<br>
-     * <code style="color: mediumseagreen; background-color: #eaffe0;">200 OK
-     * Devuelve la ciudad buscada.</code> 
-     * <code style="color: #c7254e; background-color: #f9f2f4;">404 Not Found
-     * No se pudo acceder a la base de datos de ciudades, o no existe la ciudad buscada.</code> 
-     * @param id Identificador de la ciudad que se esta buscando
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve la ciudad correspondiente al id.
+     * </code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found No existe una ciudad con el id dado.
+     * </code> 
+     * </pre>
+     * @param id Identificador de la ciudad que se esta buscando. Este debe ser una cadena de dígitos.
      * @return JSON {@link CityDetailDTO} - La ciudad buscada
      */
     @GET
@@ -117,20 +127,22 @@ public class CityResource {
     }
     
     /**
-     * <h1>PUT /api/cities/{id} : Actualizar ciudad</h1>
-     * <p>Cuerpo de peticion: JSON {@link CityDetailDTO}</p>
+     * <h1>PUT /api/cities/{id} : Actualizar ciudad con el id dado.</h1>
+     * <pre>Cuerpo de petición: JSON {@link CityDetailDTO}.
      * 
-     * <p>Actualiza la ciudad con el ID recibido en la URL con la informacion que se recibe en el cuerpo de la peticion.</p>
+     * Actualiza la ciudad con el id recibido en la URL con la informacion que se recibe en el cuerpo de la petición.
      * 
-     * Codigos de respuesta<br>
-     * <code style="color: mediumseagreen; background-color: #eaffe0;">200 OK
-     * Se actualiza la ciudad y se regresa un objeto identico.</code> 
-     * <code style="color: #c7254e; background-color: #f9f2f4;">412 Precodition Failed
-     * No existe la ciudad.</code> 
-     * @param id Identificador de la ciudad que se desea actualizar.
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Actualiza la ciudad con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una ciudad con el id dado.
+     * </code> 
+     * </pre>
+     * @param id Identificador de la ciudad que se desea actualizar.Este debe ser una cadena de dígitos.
      * @param city {@link CityDetailDTO} La ciudad que se desea guardar.
      * @return JSON {@link CityDetailDTO} - La ciudad guardada.
-     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de logica que se genera al no poder actualizar la ciudad.
+     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de lógica que se genera al no poder actualizar la ciudad porque ya existe una con ese nombre.
      */
     @PUT
     @Path("{id: \\d+}")
@@ -139,21 +151,22 @@ public class CityResource {
     }
     
     /**
-     * <h1>DELETE /api/cities/{id} : Borrar ciudad por id</h1>
+     * <h1>DELETE /api/cities/{id} : Borrar ciudad por id.</h1>
      * 
-     * <p>Borra la ciudad con el ID asociado recibido en la URL.</p>
+     * <pre>Borra la ciudad con el id asociado recibido en la URL.
      * 
-     * Codigos de respuesta<br>
-     * <code style="color: mediumseagreen; background-color: #eaffe0;">200 OK
-     * Se elimino la ciudad.</code>
-     * <code style="color: #c7254e; background-color: #f9f2f4;">404 Not Found
-     * No se pudo acceder a la base de datos de ciudades, o no existe la ciudad buscada.
-     * @param id Identificador de la ciudad que se desea borrar</code>
-     * @throws BusinessLogicException - {@link BusinessLogicExceptionMapper} Error de logica cuando no se encuentra la ciudad.
+     * Códigos de respuesta:<br>
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Elimina la ciudad correspondiente al id dado.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una ciudad con el id dado.
+     * </code>
+     * </pre>
+     * @param id Identificador de la ciudad que se desea borrar. Este debe ser una cadena de dígitos.
      */
     @DELETE
     @Path("{id: \\d+}")
-     public void deleteCity(@PathParam("id") Long id) throws BusinessLogicException {
+     public void deleteCity(@PathParam("id") Long id) {
         // Void
     }
 }
